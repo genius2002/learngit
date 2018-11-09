82b17a35ce22a375892162c4521aff3de44578ce
weston <weston.travolta@hotmail.com>
2018-11-09 18:08:05 +0800

    Init project

---
+++ YSS-PMS/src/main/java/com/ysstech/weston/pms/YssPmsApplication.java
@@ -0,0 +1,12 @@
+package com.ysstech.weston.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YssPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YssPmsApplication.class, args);
+	}
+}
---
+++ YSS-PMS/src/test/java/com/ysstech/weston/pms/YssPmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ysstech.weston.pms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YssPmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
